1e48409e55b52882c8cff9b91bb0876b0d22dbd0
Nina Yang <ssssrxdd@gmail.com>
2022-04-22 13:32:10 -0400

    ini

--- pages/components/navbar/index.js
+++ pages/components/navbar/index.js
@@ -5,6 +5,8 @@ import Image from "next/image";
 
 export default function NavBar() {
     return (<div className={styles.nav_body}>
+        <div className={styles.content}>
         <Image className={styles.logo_img} src={Logo}/>
+        </div>
     </div>)
 }
